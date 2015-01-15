8390b50c4d2f17abd4c807af15397c511e301a38
huzhennan <huzhennan2012@gmail.com>
2015-01-15 10:42:43 +0800

    ADD rxjava|lambda

--- app/src/main/java/com/hzn/sample/fragments/LoginFragment.java
+++ app/src/main/java/com/hzn/sample/fragments/LoginFragment.java
@@ -8,12 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.hzn.sample.R;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.app.AppObservable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by hzn on 15-1-14.
@@ -34,7 +41,27 @@ public class LoginFragment extends Fragment {
 
     @OnClick(R.id.bt_login)
     public void login() {
-
+        AppObservable.bindFragment(this,
+                Observable.create(new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        subscriber.onNext("hello");
+                        subscriber.onCompleted();
+                    }
+                }))
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Toast.makeText(getActivity(), "run success", Toast.LENGTH_LONG).show();
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                Toast.makeText(getActivity(), "error", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
